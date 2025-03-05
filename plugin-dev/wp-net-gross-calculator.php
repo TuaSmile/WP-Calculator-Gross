b6c14903266ceab043e56193f94ed41e68d03efa
TuaSmile <tuasmiledev@outlook.com>
2025-03-05 06:52:12 +0200

    update php php file

--- plugin-dev/wp-net-gross-calculator.php
+++ plugin-dev/wp-net-gross-calculator.php
@@ -4,9 +4,6 @@
  * Plugin URI: https://github.com/s-andrzejewski/wp-net-gross-calculator
  * Description: A plugin that calculates gross and tax amounts and saving data to CPT.
  * Version: 1.0.0
- * Author: Szymon Andrzejewski
- * Author URI: https://www.linkedin.com/in/szymon-andrzejewski-kepno/?locale=en_US
- * Text Domain: net-gross-calc
  * Domain path: /languages
  * Requires at least: 5.8
  * Requires PHP: 7.4
